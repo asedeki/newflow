fd7ae2fa1f71224f736e161c9c57b25e1988456c
Abdelouahab Sedeki <asedeki@gmail.com>
2019-09-25 12:37:26 +0200

    - modification de passages de parametres a Loops - modification initilize dans Loops - modification get_values dans Loops

--- src/loops.py
+++ src/loops.py
@@ -21,86 +21,88 @@ except Exception:
 
 
 class Loops():
-    __KEYS = set(["Np", "tp2", "tp", "Ef"])
-    __KEYS_INT = set(["Temperature", "lrg"])
-    Temperature = 1e-80
-    parameters = {k: None for k in __KEYS}
-    parameters.update({k: None for k in __KEYS_INT})
+    def __new__(cls, *args, **kwargs):
+        instance = super(Loops, cls).__new__(cls)
+        instance.__init()
+        return instance
 
-    def __init__(self, parameters: dict) -> None:
-        try:
-            Temp = difflib.get_close_matches("T", parameters, n=1)[0]
-            self.Temperature = float(parameters[Temp])
-        except Exception:
-            pass
+    @classmethod
+    def __init(cls):
+        cls.__KEYS = set(["Np", "tp2", "tp", "Ef"])
+        cls.__KEYS_INT = set(["Temperature", "lrg"])
+        cls.Temperature = None
 
-        matched_parameters = best_match_dict(
-            parameters, Loops.__KEYS
-        )
-        miss = [pa for pa in Loops.__KEYS if pa not in matched_parameters]
-        miss.sort()
-        if not miss:
-            self.param = matched_parameters
-        else:
-            message = "{} must have {} keys".format(
-                parameters.__repr__(), ','.join(miss))
-            raise KeyError(message)
+        cls.parameters = {k: None for k in cls.__KEYS}
+        cls.parameters["lrg"] = None
+        cls.parameters["Temperature"] = 1e-80
+        cls.Cooper = None
+        cls.Peierls = None
+        cls.Peierls_susc = None
 
-        Np = int(self.param["Np"])
-        self.param["Temperature"] = self.Temperature
+    def __init__(self, parameters: dict = None) -> None:
 
-        self.Cooper = np.zeros((Np, Np, Np), float)
-        self.Peierls = np.zeros((Np, Np, Np), float)
-        self.Peierls_susc = np.zeros((Np, 2), float)
+        if parameters is not None:
+            matched_parameters = best_match_dict(
+                parameters, Loops.__KEYS.union(Loops.__KEYS_INT)
+            )
+            miss = [pa for pa in Loops.__KEYS if pa not in matched_parameters]
+            miss.sort()
+            # # input(f"before = {self.parameters}")
+            # # input(f"match = {matched_parameters}")
+            self.parameters.update(matched_parameters)
+            # # input(f"after = {self.parameters}")
 
-    def initialize(self, **kwargs: dict) -> object:
+            if miss:
+                message = "{} must have {} keys".format(
+                    parameters.__repr__(), ','.join(miss))
+                raise KeyError(message)
+
+            Np = int(self.parameters["Np"])
+            self.Temperature = self.parameters["Temperature"]
+
+    def initialize(self, **kwargs: dict) -> None:
         '''
             Set Loops values to null.
 
         '''
-        if kwargs is not None:
-            if "Temperature" in kwargs:
-                self.Temperature = kwargs["Temperature"]
-                del kwargs["Temperature"]
-            else:
-                self.Temperature = Loops.Temperature
-            if kwargs:
-                dic = best_match_dict(
-                    kwargs, Loops.__KEYS
-                )
-                # input(f"dic = {dic}")
-                self.param.update(dic)
-        self.param["Temperature"] = self.Temperature
-        self.Cooper = np.zeros(self.Cooper.shape, float)
-        self.Peierls = np.zeros(self.Peierls.shape, float)
-        self.Peierls_susc = np.zeros(self.Peierls_susc.shape, float)
-        return self
 
-    def get_values(self, l_rg: float):
+        if kwargs:
+            dic = best_match_dict(
+                kwargs, set(self.parameters.keys())
+            )
+            # # # input(f"dic = {dic}")
+            self.parameters.update(dic)
+        # input(f"param_in_intialize = {self.parameters}")
+        self.Temperature = self.parameters["Temperature"]
+        assert(self.parameters["Np"] is not None)
+        shape = (self.parameters["Np"], self.parameters["Np"],
+                 self.parameters["Np"])
+        self.Cooper = np.zeros(shape, float)
+        self.Peierls = np.zeros(shape, float)
+        self.Peierls_susc = np.zeros((self.parameters["Np"], 2), float)
 
-        # cb.loops_integration(l_rg, self.Temperature, self.param, self.Cooper,
+    def get_values(self, **kwargs: dict):
+
+        # cb.loops_integration(l_rg, self.Temperature, self.parameters, self.Cooper,
         #                      self.Peierls, self.Peierls_susc)
-        self.param["lrg"] = l_rg
-        cb.loops_integration(
-            self.param, self.Cooper, self.Peierls, self.Peierls_susc
-        )
-        del self.param["lrg"]
+        self.initialize(**kwargs)
+        self.__call__()
 
-    def __call__(self, l_rg: float):
+    def __call__(self, l_rg: float = None):
         '''
             Calculate the values of Cooper and Peierls loops
-            for a given Temperature and RG_l parameter.
+            for a given Temperature and RG_l parameterseter.
             This is donne via cbulle module using cython
             to do integration for time considerations.
 
         '''
+        if l_rg is not None:
+            self.parameters["lrg"] = l_rg
         try:
-            self.param["lrg"] = l_rg
             cb.loops_integration(
-                self.param, self.Cooper,
+                self.parameters, self.Cooper,
                 self.Peierls, self.Peierls_susc
             )
-            del self.param["lrg"]
         except Exception as identifier:
             print(identifier)
             return False
--- test/test_loops.py
+++ test/test_loops.py
@@ -8,7 +8,7 @@ try:
     from ..src.loops import Loops
 except ImportError:
     python_path = pathlib.posixpath.abspath("..")
-    # # input(python_path)
+    # # # input(python_path)
     sys.path.append(python_path)
     from src.loops import Loops
 except ValueError:
@@ -29,13 +29,17 @@ class TestLoops(unittest.TestCase):
             "tp": 200, "tp2": 20,
             "Ef": 3000, "Np": 32
         }
+        self.loops = Loops()
 
     def tearDown(self):
-        pass
+        p = Loops()
+        # input(f"self_para = {self.parameters}")
+        p.initialize(**self.parameters)
+        # input(p.parameters)
 
     def test_init(self):
-        keys = list(self.parameters.keys())
-        n = randint(1, len(keys)+1)
+        keys = ["tp", "tp2", "Ef"]
+        n = randint(1, 2)
         not_miss = set([])
         for _ in range(n):
             not_miss.add(choice(keys))
@@ -46,75 +50,78 @@ class TestLoops(unittest.TestCase):
                 miss.append(key)
 
         miss.sort()
-        # # input(miss)
-        # # input(self.parameters)
+
         message = "{} must have {} keys".format(
             self.parameters.__str__(), ','.join(miss))
-        self.assertRaisesRegex(KeyError, message,
-                               Loops, self.parameters)
-
+        try:
+            self.assertRaisesRegex(KeyError, message,
+                                   Loops, self.parameters)
+        except AssertionError:
+            print(f"message attendu = {message}")
+            print(f"parametres loops = {Loops.parameters}")
+            raise AssertionError
         parameters = {"tp": 200, "tp2": 20, "Ef": 3000, "Np": 32}
         result = {'tp': 200, 'tp2': 20, 'Ef': 3000,
-                  'Np': 32, 'Temperature': 1e-80}
+                  'Np': 32, 'Temperature': 1e-80, "lrg": None}
+
         loops = Loops(parameters)
-        self.assertDictEqual(result, loops.param)
+        self.assertDictEqual(result, loops.parameters)
 
     def test_call(self):
         parameters = {"tp": 200, "tp2": 20, "Ef": 3000, "Np": 32}
-        # input(parameters)
-        loops = Loops(parameters)
+        # # input(parameters)
         Temperature = 1.0
         l_rg = 1.0
         # loops.initialize(Temperature=Temperature)
         # loops(l_rg)
-        loops.initialize(Temperature=Temperature)(l_rg)
-        a = (np.sum(loops.Cooper), np.sum(loops.Peierls),
-             np.sum(loops.Peierls_susc))
+        self.loops.initialize(**parameters,
+                              Temperature=Temperature,
+                              lrg=l_rg)
+        self.loops()
+        # input(self.loops.parameters)
+        a = (np.sum(self.loops.Cooper), np.sum(self.loops.Peierls),
+             np.sum(self.loops.Peierls_susc))
         b = (878.6685890125864, 878.5183879970207, 1.7964371318299874)
 
         self.assertEqual(a, b)
-        # # input(loops.param)
-
+        # # # input(loops.parameters)
+        loops = self.loops
         Temperature = 10.0
         l_rg = 10.0
-        loops.loops_integration_donne = False
-        loops.initialize(Temperature=Temperature)
-        loops(l_rg)
+        loops.initialize(Temperature=Temperature, lrg=l_rg)
+        loops()
         a = (np.sum(loops.Cooper), np.sum(
             loops.Peierls), np.sum(loops.Peierls_susc))
         b = (2.5118151628507546, 2.6610631305629457, 0.007343458492368056)
         self.assertEqual(a, b)
-        # # input(loops.param)
+        # # # input(loops.parameters)
 
         Temperature = 1e-10
         l_rg = 10.0
         loops.initialize(Temperature=Temperature)
-        loops.loops_integration_donne = False
         loops(l_rg)
         a = (np.sum(loops.Cooper), np.sum(
             loops.Peierls), np.sum(loops.Peierls_susc))
         b = (37.949254425810715, 8.986533868805642, 0.015657680353671738)
         self.assertEqual(a, b)
 
+        Temperature = 1e-80
         l_rg = 20.0
-        loops.initialize()
-        loops.loops_integration_donne = False
-        loops(l_rg)
+        loops.initialize(Temperature=Temperature, lrg=l_rg)
+        loops()
         a = (np.sum(loops.Cooper), np.sum(
             loops.Peierls), np.sum(loops.Peierls_susc))
-        # # input(loops.param)
+        # # # input(loops.parameters)
         # print(a)
         b = (32.00127317596035, 0.0065097990057401195, 1.7901397210660127e-06)
         self.assertEqual(loops.Temperature, 1e-80)
         self.assertEqual(a, b)
 
         l_rg = 20.0
-        loops.initialize(tp=0.0, tp2=0.0, Np=8)
-        loops.loops_integration_donne = False
-        loops(l_rg)
+        loops.get_values(tp=0.0, tp2=0.0, Np=8, lrg=l_rg)
         a = (np.sum(loops.Cooper), np.sum(
             loops.Peierls))
-        Np = loops.param["Np"]
+        Np = loops.parameters["Np"]
         b = (Np**2, Np**2)
         self.assertEqual(loops.Temperature, 1e-80)
         for i, j in zip(a, b):
